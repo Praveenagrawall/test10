4eeb3fce6e55ce603ab3f1d47fadbc338980921f
Praveenagrawall <praveenagrawal4073@gmail.com>
2025-03-16 10:09:03 +0530

    v

--- src/main/java/com/gittest/GittestApplication.java
+++ src/main/java/com/gittest/GittestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GittestApplication {
 
 	public static void main(String[] args) {
+boolean = true;
 		SpringApplication.run(GittestApplication.class, args);
 	}
 
